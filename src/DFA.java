60245402551b21b3c5fd843da4df3ca3fef7adf3
aschmi34 <aschmi34@students.towson.edu>
2022-03-10 14:38:48 -0500

    Initial commit

---
+++ src/DFA.java
@@ -0,0 +1,31 @@
+public class DFA {
+    public static void main(String[] args) {
+
+
+        /*
+        Criteria:
+
+        at most 20 states
+        alphabet is {0,1}
+        outputs ACCEPT for valid strings and REJECT for invalid strings
+        starting state is 1
+
+        User input:
+
+        Ask user to enter transition function delta
+
+        ask user to enter the set of accepting states
+
+        ask user to enter input string
+
+         */
+
+
+        int[] states, accepts;
+        String input;
+        boolean ACCEPTS;
+
+        
+
+    }
+}
